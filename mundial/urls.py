cb6b045cfa767f278dcad0d89e6f1ffba27cd7b6
Gino <20120169@aloe.ulima.edu.pe>
2018-01-14 23:12:36 -0500

    login

--- mundial/urls.py
+++ mundial/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import LoginView
 
 urlpatterns = [
+    path('', LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
 ]
