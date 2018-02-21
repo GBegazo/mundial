f0f1a1b4ed68b4595ac40a4d01ea83d94f7f5f3d
Gino <20120169@aloe.ulima.edu.pe>
2018-02-21 17:32:38 -0500

    cambios realizados en el juego de la polla

--- app/juegosmundial/urls.py
+++ app/juegosmundial/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.conf.urls import url
 
 from app.juegosmundial.views import index_juegosmundial,equipoidealjuegos,juegosmundialbien,triviadescrip,\
-                            equipoidealdescrip,polladescrip,triviajuegos,preguntas_list,listado,triviafinal,listadojugadores,equipo_view,equiposguardados,apuesta,grupoB,grupoA,grupoC,grupoD,grupoE,grupoF,grupoG,grupoH,validar
+                            equipoidealdescrip,polladescrip,triviajuegos,preguntas_list,listado,triviafinal,\
+                            listadojugadores,equipo_view,equiposguardados,apuesta,grupoB,grupoA,grupoC,grupoD,\
+                            grupoE,grupoF,grupoG,grupoH,validar,listarpartidos,saveapuesta,finddinero,solvepartido
 
 app_name = 'juegosmundial'
 
@@ -24,7 +26,6 @@ urlpatterns = [
     url(r'^equiposguardados/',equiposguardados, name='equiposguardados'),
     #agregando apuesta
     url(r'^apuesta/',apuesta, name='apuesta'),
-
     url(r'^grupoa/',grupoA, name='grupoA'),
     url(r'^grupob/',grupoB, name='grupoB'),
     url(r'^grupoc/',grupoC, name='grupoC'),
@@ -33,6 +34,18 @@ urlpatterns = [
     url(r'^grupof/',grupoF, name='grupoF'),
     url(r'^grupog/',grupoG, name='grupoG'),
     url(r'^grupoh/',grupoH, name='grupoH'),
+
+    #definir los partidos
+    url(r'^listarpartidos/',listarpartidos, name='listarpartidos'),
+
+    #guardar apuesta
+    url(r'^saveapuesta/',saveapuesta, name='saveapuesta'),
+
+    #Calcular dinero
+    url(r'^finddinero/',finddinero, name='finddinero'),
+
+    #Resolver partido
+    url(r'^solvepartido/',solvepartido, name='solvepartido'),
 
 
     
